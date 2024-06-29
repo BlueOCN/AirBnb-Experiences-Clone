4713ca5123389b65d7fc424431881811cd7664fa
BlueOCN <robgonzalezmu@gmail.com>
2024-06-28 19:10:34 -0600

    Layout Update

--- AirBnbExperiencesClone/src/App.jsx
+++ AirBnbExperiencesClone/src/App.jsx
@@ -1,15 +1,27 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data'
 import './App.css'
 
 function App() {
+
+  const deck = data.map(data => {
+    return (
+      <Card
+        key={data.id}
+        items={data}
+      />
+    )
+  })
 
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <section className='deck-container'>
+        {deck}  
+      </section>
 
     </>
   )
--- AirBnbExperiencesClone/src/components/Card.jsx
+++ AirBnbExperiencesClone/src/components/Card.jsx
@@ -1,6 +1,19 @@
 
 function Card (props) {
-    
+    console.log(props.items)
+    return (
+        <div className="card">
+            <img className="card--img" src={"../src/assets/"+props.items.coverImg} alt="cover_img"/>
+            <div className="card--stats">
+                <img className="stats--img" src="../src/assets/Star.png" alt="star_img" />
+                <p className="stats--rating">{props.items.stats.rating}</p>
+                <p className="stats--reviewCount">({props.items.stats.reviewCount}) ⋅ </p>
+                <p className="stats--location"> {props.items.location}</p>
+            </div>
+            <h2 className="normal">{props.items.title}</h2>
+            <p className="normal"><span className="bold">From ${props.items.price}</span> / person</p>
+        </div>
+    )
 }
 
 export default Card
